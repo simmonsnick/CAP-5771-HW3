fb6b70b8c58313ece4055e96dec5e138a43b68e0
Nicholas <93218182+simmonsnick@users.noreply.github.com>
2024-03-04 21:05:39 -0500

    Update all_questions.py

--- all_questions.py
+++ all_questions.py
@@ -1,3 +1,7 @@
+import pickle
+
+
+# -----------------------------------------------------------
 def question1():
     answers = {}
     # a) Agglomerative hierarchical clustering procedures are better able to handle outliers than k-means.
@@ -5,16 +9,16 @@ def question1():
     answers["(a)"] = True
 
     # type: explanatory string (at least four words)
-    answers["(a) explain"] = "In Agglomerative clustering, outliers are less problematic and do not merge with any other clusters."
-    "K-means has problems when data contain outliers."
+    answers["(a) explain"] = "In Agglomerative clustering, outliers are less problematic and it does not merge with any other clusters."
+    "K-means has trouble clustering data that contain outliers."
 
     # For any given data set, different runs of k-means can produce different clusterings, but agglomerative hierarchical clustering procedures will always produce the same clustering.
     # type: bool (True/False)
     answers["(b)"] = True
 
     # type: explanatory string (at least four words)
-    answers["(b) explain"] = "K-means has a number of limitations with finding different types of clusters."
-    "Agglomonerative clustering merges the closest pair but does not randomize the data points in each cluster."
+    answers["(b) explain"] = "K-means has a number of limitations in finding different types of clusters im multiple runs."
+    "Agglomonerative clustering merges the closest pairs but does not randomize the data points in each cluster."
     
     # K-means take less time and memory than agglomerative hierarchical clustering and is the most efficient clustering algorithm possible.
     # type: bool (True/False)
@@ -38,7 +42,7 @@ def question1():
     answers["(e)"] = True
 
     # type: explanatory string (at least four words)
-    answers["(e) explain"] = "In K-means, SSE has a measure of the cohesion in clusters."
+    answers["(e) explain"] = "In K-means, Square Sum Error has a measure of the cohesion in clusters."
     
     # When clustering a dataset using K-means, whenever SSB (the between sum of squares) increases, separation increases.
     # type: bool (True/False)
@@ -59,16 +63,18 @@ def question1():
     answers["(h)"] = False
 
     # type: explanatory string (at least four words)
-    answers["(h) explain"] = "The sum of the total SSE and the total SSB is a constant, SSE + SSB"
+    answers["(h) explain"] = "The sum of the total SSE and the total SSB is a constant, SSE + SSB."
     
     #When clustering a dataset using K-means, whenever cohesion increases, separation increases.
     # type: bool (True/False)
     answers["(i)"] = False
 
     # type: explanatory string (at least four words)
-    answers["(i) explain"] = "The result is that minimizing SSE (cohesion) is equivalent to maximizing SSB (separation)"
+    answers["(i) explain"] = "The result is that minimizing SSE cohesion is equivalent to maximizing SSB separation."
 
     return answers
+
+
 
 # -----------------------------------------------------------
 def question2():
@@ -84,13 +90,13 @@ def question2():
     answers["(b)"] = False
 
     # type: explanatory string (at least four words)
-    answers["(b) explain"] = "The shaded regions are near to each other and not of circular shape or size."
+    answers["(b) explain"] = "The shaded regions are near to each other and not in a round shape."
 
     # type: bool (True/False)
     answers["(c)"] = True
 
     # type: explanatory string (at least four words)
-    answers["(c) explain"] = "In Figure C, 12.5 seems to have point that is farthest away from any current cluster."
+    answers["(c) explain"] = "In Figure C, has a point that's far away from any current cluster."
 
     return answers
 
@@ -104,12 +110,13 @@ def question3():
     
     # Compute the total SSE of the data points to the origin, O.
     # type: a string that evaluates to a float
-    answers["(b) SSE"] = "4(a^2 + b^2 + R^2)"
+    answers["(b) SSE"] = "4*(a^2 + b^2 + R^2)"
     
     # type: a string that evaluates to a float
     answers["(c) SSE"] = "10R^2"
-
+    #calculate all the regions that consist the points
     return answers
+
 
 # -----------------------------------------------------------
 def question4():
@@ -153,6 +160,7 @@ def question4():
 
     return answers
 
+
 # -----------------------------------------------------------
 def question5():
     answers = {}
@@ -178,21 +186,24 @@ def question6():
 
     # type: set
     answers["(a) core"] = set(['B', 'C', 'E', 'F', 'I', 'J', 'L', 'M'])
+    #core is the interior of a density-based cluster
 
     # type: set
     answers["(a) boundary"] = set(['D', 'G'])
+    #border is not a core point, falls with the neighbor of a core point
 
     # type: set
     answers["(a) noise"] = set(['A', 'H'])
+    #noise any point that's neither a core or border
 
     # type: set
     answers["(b) cluster 1"] = set(['B', 'C', 'D', 'E', 'F', 'G'])
-    #Since point A is the farthest I would consider the closest points in the first cluster.
+    #Since point A is the farthest I would choose the closest points in the first cluster.
     #This cluster has more data points.
 
     # type: set
     answers["(b) cluster 2"] = set(['I', 'J', 'L', 'M'])
-    #Since point H is the farthest I would consider the closest points in the second cluster.
+    #Since point H is the farthest I would choose the closest points in the second cluster.
 
     # type: set
     answers["(b) cluster 3"] = set()
@@ -228,7 +239,7 @@ def question6():
     # There's no fourth cluster in figure 5
 
     return answers
-
+    
 
 # -----------------------------------------------------------
 def question7():
@@ -257,7 +268,7 @@ def question8():
     answers["(a) Matrix 1"] = "Dataset Z"
 
     # type: explanatory string (at least four words)
-    answers["(a) explain diag entries, Matrix 1"] = "Has one diagonal entry that's blue."
+    answers["(a) explain diag entries, Matrix 1"] = "One diagonal entry that's blue."
 
     # type: explanatory string (at least four words)
     answers["(a) explain non-diag entries, Matrix 1"] = "It shows different colors in the non diagonal entries."
@@ -266,23 +277,23 @@ def question8():
     answers["(a) Matrix 2"] = "Dataset X"
 
     # type: explanatory string (at least four words)
-    answers["(a) explain diag entries, Matrix 2"] = "Two diagonal entries are more blue and light blue compared to others."
+    answers["(a) explain diag entries, Matrix 2"] = "Two diagonal entries are more blue and light blue compared to other patterns."
 
     # type: explanatory string (at least four words)
-    answers["(a) explain non-diag entries, Matrix 2"] = "Rows that are not light blue has different colors with non diagonal entries."
+    answers["(a) explain non-diag entries, Matrix 2"] = "Rows that are not light blue has different colors with non-diagonal entries."
 
     # type: string
     answers["(a) Matrix 3"] = "Dataset Y"
 
     # type: explanatory string (at least four words)
-    answers["(a) explain diag entries, Matrix 3"] = "Has a blue diagonal entries that's compared to the previous two."
+    answers["(a) explain diag entries, Matrix 3"] = "The blue diagonal entry that is compare to the previous two."
 
     # type: explanatory string (at least four words)
-    answers["(a) explain non-diag entries, Matrix 3"] = "Most rows are similar but one different color are non diagonal. "
-
+    answers["(a) explain non-diag entries, Matrix 3"] = "One row has a different color is non-diagonal. "
+    #cluster cohesion how closely related the objects in a cluster
     # type: string
     answers["(b) Row 1"] = "1-100 cluster"
-
+    #set of points contains 100 points
     # type: string
     answers["(b) Row 2"] = "101-200 cluster"
 
@@ -291,10 +302,10 @@ def question8():
 
     # type: string
     answers["(b) Row 4"] = "301-400 cluster"
-
+  
     # type: explanatory string (at least four words)
     answers["(b) Row 1 explain"] = "Cluster is less cohesive in diagonal entry, clusters have different distances."
-
+    
     # type: explanatory string (at least four words)
     answers["(b) Row 2 explain"] = "Cluster is more cohesive, entries have the same color which the cluster are near each other."
 
@@ -327,9 +338,11 @@ def question9():
     answers["(e)"] = ['Partitional', 'Exclusive', 'Partial']
 
     # type: explanatory string (at least four words)
-    answers["(e) explain"] = "Can't be grouped with some student didn't get in the data mining class."
+    answers["(e) explain"] = "Can't be grouped with students didn't get in by grade in data mining."
 
     return answers
+
+
 
 
 # -----------------------------------------------------------
@@ -337,25 +350,25 @@ def question10():
     answers = {}
 
     # type: string
-    answers["(a) Figure (a)"] = "The noise is much denser than the interest patterns so noise, eyes, and mouth will be out."
-
+    answers["(a) Figure (a)"] = "The noise is much dense than the specific patterns so eyes and mouth will be out."
+    
     # type: string
-    answers["(a) Figure (b)"] = "The points in nose, eyes, and mouth are much closer together and DBSCAN can work for this task."
+    answers["(a) Figure (b)"] = "The points in nose, eyes, and mouth are nearby and DBSCAN works for the figure b."
 
     # type: explanatory string (at least four words)
-    answers["(a) explain"] = "DBSCAN can be use for every figure to identify the clusters."
+    answers["(a) explain"] = "DBSCAN can be use to identify the clusters."
 
     # type: string
-    answers["(b) Figure (a)"] = "In this figure, K-means does not work for figure a."
+    answers["(b) Figure (a)"] = "In figure, K-means does not work for figure a."
 
     # type: string
-    answers["(b) Figure (b)"] = "In this figure, K-means does work for figure b."
+    answers["(b) Figure (b)"] = "In figure, K-means does work for figure b."
 
     # type: explanatory string (at least four words)
-    answers["(b) explain"] = "You can't use K-mean for every figure with the placement in certain data point to the cluster."
+    answers["(b) explain"] = "You can't use K-mean for every figure with the placement in certain data points to the cluster."
 
     # type: string
-    answers["(c)"] = "The reciprocal of the density being use in DBSCAN."
+    answers["(c)"] = "The density-based measure being use in DBSCAN can find the patterns in nose, eyes, and mouth."
 
     return answers
 
